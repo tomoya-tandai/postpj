a9dcbdc370c27c62cf07000a034e08daf78371b3
tomoya-tandai <tomoya.tandai26@gmail.com>
2020-09-09 10:47:02 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <input type="text" maxlength="7" v-model="text">
+    <button @click="created()">住所自動入力</button>
+    <p>Address:{{address}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return{
+      address:"",
+      text:""
+    }
+  },
+  methods:{
+    async created(){
+      console.log(this.text);
+      let item = await axios.get(
+      `https://apis.postcode-jp.com/api/v3/postcodes/${this.text}?apikey=Gq4hN0Af636GO5Rlt42CIPsp5vSmMp1re0jDqgR`
+      );
+      this.data = item.data;
+      this.address = this.data.allAddress;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
